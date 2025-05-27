ce8c22e6cc54dbe7bf238b3c618f649e5d6987d8
geoka <geokaraviotis@gmail.com>
2025-05-27 14:39:38 +0300

    server receives andd client sends now

--- client_win.c
+++ client_win.c
@@ -8,9 +8,9 @@
 #include <ws2tcpip.h>
 #include <time.h>
 
-#pragma comment(lib, "ws2_32.lib")  // Link against Winsock library
+#pragma comment(lib, "ws2_32.lib")
 
-#define BUFFER_SIZE 10000
+#define BUFFER_SIZE 1024
 #define DURATION 30
 #define INTERVAL 2
 
@@ -27,31 +27,29 @@ int main(int argc, char *argv[]) {
     SOCKET sockfd;
     struct sockaddr_in server_addr;
     char *buffer = (char *)malloc(BUFFER_SIZE);
+    memset(buffer, 'A', BUFFER_SIZE);  // Fill with dummy data
+
     if (!buffer) {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
 
-    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup failed: %d\n", WSAGetLastError());
         return 1;
     }
 
-    // Create socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
         fprintf(stderr, "Socket creation failed: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-    // Setup server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(server_ip);  // Works for dotted IPv4
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
-    // Connect to server
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
         fprintf(stderr, "Connect failed: %d\n", WSAGetLastError());
         closesocket(sockfd);
@@ -65,29 +63,29 @@ int main(int argc, char *argv[]) {
     time_t next_report = start_time + INTERVAL;
     time_t end_time = start_time + DURATION;
 
-    size_t total_bytes_received = 0;
-    size_t interval_bytes_received = 0;
+    size_t total_bytes_sent = 0;
+    size_t interval_bytes_sent = 0;
 
     while (time(NULL) < end_time) {
-        int bytes = recv(sockfd, buffer, BUFFER_SIZE, 0);
-        if (bytes <= 0) {
-            fprintf(stderr, "recv failed or connection closed: %d\n", WSAGetLastError());
+        int sent = send(sockfd, buffer, BUFFER_SIZE, 0);
+        if (sent <= 0) {
+            fprintf(stderr, "Send failed: %d\n", WSAGetLastError());
             break;
         }
 
-        total_bytes_received += bytes;
-        interval_bytes_received += bytes;
+        total_bytes_sent += sent;
+        interval_bytes_sent += sent;
 
         if (time(NULL) >= next_report) {
-            double mbps = (interval_bytes_received * 8.0) / (INTERVAL * 1000000.0);
-            printf("[INTERVAL] Received %.2f Mbps\n", mbps);
-            interval_bytes_received = 0;
+            double mbps = (interval_bytes_sent * 8.0) / (INTERVAL * 1000000.0);
+            printf("[INTERVAL] Sent %.2f Mbps\n", mbps);
+            interval_bytes_sent = 0;
             next_report += INTERVAL;
         }
     }
 
-    double total_mbps = (total_bytes_received * 8.0) / (DURATION * 1000000.0);
-    printf("[TOTAL] Received %.2f Mbps in %d seconds\n", total_mbps, DURATION);
+    double total_mbps = (total_bytes_sent * 8.0) / (DURATION * 1000000.0);
+    printf("[TOTAL] Sent %.2f Mbps in %d seconds\n", total_mbps, DURATION);
 
     closesocket(sockfd);
     WSACleanup();
